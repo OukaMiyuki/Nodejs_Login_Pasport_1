22ff39c81f706917d3cca1530888d746baf53e8e
OukaMiyuki <asukaloid.blog@gmail.com>
2020-08-05 11:41:33 +0700

    Penambahan keterangan auth.js

--- Config/auth.js
+++ Config/auth.js
@@ -1,14 +1,14 @@
  
 module.exports = {
     ensureAuthenticated: function(req, res, next) {
-        if (req.isAuthenticated()) {
+        if (req.isAuthenticated()) {//if user not login
             return next();
         }
         req.flash('gagal', 'Please log in to view that resource');
         res.redirect('/users/login');
     },
     forwardAuthenticated: function(req, res, next) {
-        if (!req.isAuthenticated()) {
+        if (!req.isAuthenticated()) {//if user is logged in
             return next();
         }
         res.redirect('/dashboard');      
